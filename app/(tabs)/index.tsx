506e189096ed2084733c9dfed52a16d70d0c9284
artiphishle <fabio.gartenmann@protonmail.com>
2024-11-15 03:41:09 +0100

    Fix types import

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Surface, Text } from 'react-native-paper'
+import { ElevationLevels } from 'react-native-paper/src/types'
 
 import Locales from '@/lib/locales'
 import { ScreenInfo, styles } from '@/lib/ui'
 import SearchBar from '@/lib/ui/components/SearchBar'
-import { ElevationLevels } from 'react-native-paper/lib/typescript/types'
 
 const TabsHome = () => (
   <>
-    <SearchBar value='' placeholder='Search LIchess user' />
+    <SearchBar value="" placeholder="Username" />
     <Surface elevation={ElevationLevels.level0} style={styles.screen}>
       <ScreenInfo title={Locales.t('titleHome')}>
         <Text>{Locales.t('contentToFollow')}</Text>
